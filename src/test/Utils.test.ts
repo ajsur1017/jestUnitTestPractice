da0d51fb5f617c110b66e89c90117f4ea9697e96
Anthony <ajsur1017@gmail.com>
2022-08-05 13:05:53 -0400

    unit testing practice

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -6,11 +6,21 @@ describe('Utils test suite', () => {
     expect(result).toBe('ABC')
   });
 
-  test('pares simple URL', () => {
-    const parsedUrl = Utils.parseURL('http://localhost8080/login');
-    expect(parsedUrl.href).toBe('http://localhost8080/login');
+  test('parse simple URL', () => {
+    const parsedUrl = Utils.parseURL('http://localhost:8080/login');
+    expect(parsedUrl.href).toBe('http://localhost:8080/login');
     expect(parsedUrl.port).toBe('8080');
-    expect(parsedUrl.protocol).toBe('http');
+    expect(parsedUrl.protocol).toBe('http:'); // toBe is used to test primitave values
+    expect(parsedUrl.query).toEqual({}); // toEqual is used to test objects
+  });
 
+  test('parse URL with query', () => {
+    const parsedUrl = Utils.parseURL('http://localhost:8080/login?user=user&password=pass');
+    const expectedQuery = {
+      user: 'user',
+      password: 'pass'
+    };
+    expect(parsedUrl.query).toEqual(expectedQuery);
+    expect(expectedQuery).toBe(expectedQuery)
   })
-})
+});
